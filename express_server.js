4417a0f75d125d38119dfec32219992dc65a0621
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-05-08 18:41:17 +0000

    Added web form and fn to create short URLs

--- express_server.js
+++ express_server.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,6 +29,15 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
